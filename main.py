a5c7a3f2da7f3657290ce02b663aacf91d50f99d
GuineaPigCode <93527295+GuineaPigCode@users.noreply.github.com>
2021-12-08 14:08:14 -0300

    py lol

---
+++ main.py
@@ -0,0 +1,19 @@
+
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = "youprefix", case_insensitive = True)
+
+#event start
+
+@client.event
+async def on_ready():
+  print('Hello World!')
+
+@client.command()
+async def template(ctx):
+  await ctx.send(f'start template')  
+
+#event end  
+
+client.run('youtoken')
